ff413c11b7df841509b1cf5f21d5c5285107fe30
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-02-19 08:31:50 +0530

    first commit

---
+++ calculator.py
@@ -0,0 +1,29 @@
+def add(x, y):
+    return x + y
+
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    choice = input("Enter choice(1/2/3/4): ")
+
+    if choice in ('1'):
+        try:
+            num1 = int(input("Enter first number: "))
+            num2 = int(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+        
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+    else:
+        print("Invalid Input")
