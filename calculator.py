acedef3ffa2fba1057a64efbd03bdf943a5d66ef
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-02-19 08:55:16 +0530

    version3

--- calculator.py
+++ calculator.py
@@ -3,6 +3,10 @@ def add(x, y):
 def subtract(x, y):
     return x - y
 
+def multiply(x, y):
+    return x * y
+
+
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
@@ -12,7 +16,7 @@ print("4.Divide")
 while True:
     choice = input("Enter choice(1/2/3/4): ")
 
-    if choice in ('1','2'):
+    if choice in ('1','2','3'):
         try:
             num1 = int(input("Enter first number: "))
             num2 = int(input("Enter second number: "))
@@ -26,6 +30,9 @@ while True:
         elif choice == '2':
             print(num1, "-", num2, "=", subtract(num1, num2))
         
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+            
         next_calculation = input("Let's do next calculation? (yes/no): ")
         if next_calculation == "no":
           break
